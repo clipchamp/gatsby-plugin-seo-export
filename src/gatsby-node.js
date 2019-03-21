56bcb89f7f09f135bb73b4c4a08907b21df73ffa
Matthew Miller <mnmiller1@me.com>
2019-03-21 14:58:35 +1000

    Use promises

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,38 +1,48 @@
-const { convertArrayToCSV } = require('convert-array-to-csv');
-const fs = require('fs');
-const cheerio = require('cheerio');
-const rimraf = require('rimraf');
+const { convertArrayToCSV } = require("convert-array-to-csv");
+const fs = require("fs");
+const cheerio = require("cheerio");
+const rimraf = require("rimraf");
 
 const templateMap = new Map();
 
 function onPostBuild(postBuild) {
-    console.log("Generating SEO Export");
-    const nodes = postBuild.getNodesByType("SitePage");
-    for (let i = 0; i < nodes.length; i++) {
-        const node = nodes[i];
-        if (!fs.existsSync(`public${node.path}/index.html`)) {
-            continue; // Skip virtual page
-        }
-        const fileContents = fs.readFileSync(`public${node.path}/index.html`)
+  console.log("Generating SEO Export");
+  Promise.all(
+    postBuild
+      .getNodesByType("SitePage")
+      .filter(node => fs.existsSync(`public${node.path}/index.html`))
+      .map(async node => {
+        const fileContents = await new Promise((resolve, reject) => {
+          fs.readFile(`public${node.path}/index.html`, (errors, data) => {
+            if (errors) {
+              reject(errors);
+            }
+            resolve(data);
+          });
+        });
         const $ = cheerio.load(fileContents);
 
         const pageList = templateMap.get(node.componentChunkName) || [];
         pageList.push({
-            path: node.path,
-            title: $("title").contents().text(),
-            description: $("meta[name='description']").attr("content")
+          path: node.path,
+          title: $("title")
+            .contents()
+            .text(),
+          description: $("meta[name='description']").attr("content")
         });
         templateMap.set(node.componentChunkName, pageList);
+      })
+  ).then(() => {
+    if (fs.existsSync("seo-export/")) {
+      rimraf.sync("seo-export");
     }
-    if (fs.existsSync('seo-export/')) { 
-        rimraf.sync('seo-export');    
-    }
-    fs.mkdirSync('seo-export');
+    fs.mkdirSync("seo-export");
     for (const [component, list] of templateMap.entries()) {
-        list.sort((a, b) => a.path.localeCompare(b.path));
-        const csv = convertArrayToCSV(list);
-        fs.writeFileSync(`seo-export/${component}.csv`, csv);
+      list.sort((a, b) => a.path.localeCompare(b.path));
+      const csv = convertArrayToCSV(list);
+      fs.writeFileSync(`seo-export/${component}.csv`, csv);
     }
+  });
 }
 
 exports.onPostBuild = onPostBuild;

193660ef7e612b368d9cfd58e0d4e547d59f46bd
Matthew Miller <mnmiller1@me.com>
2019-05-27 12:33:08 +1000

    fix(*): use a promise pool to prevent too many file handles on larger projects - publish 1.1.0

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -5,38 +5,53 @@ const rimraf = require("rimraf");
 
 const templateMap = new Map();
 
-function onPostBuild(postBuild) {
+async function onPostBuild(postBuild) {
   if (process.env.SEO_EXPORT) {
     console.log("Generating SEO Export");
-    Promise.all(
-      postBuild
-        .getNodesByType("SitePage")
-        .filter(async node => await fs.exists(`public${node.path}/index.html`.replace('//', '/')))
-        .map(async node => {
-          const fileContents = await fs.readFile(`public${node.path}/index.html`.replace('//', '/'));
-          const $ = cheerio.load(fileContents);
+    const promises = postBuild
+      .getNodesByType("SitePage")
+      .filter(node =>
+        fs.existsSync(`public${node.path}/index.html`.replace("//", "/"))
+      )
+      .map(node => async () => {
+        const fileContents = await fs.readFile(
+          `public${node.path}/index.html`.replace("//", "/")
+        );
+        const $ = cheerio.load(fileContents);
 
-          const pageList = templateMap.get(node.componentChunkName) || [];
-          pageList.push({
-            path: node.path,
-            title: $("title")
-              .contents()
-              .text(),
-            description: $("meta[name='description']").attr("content")
-          });
-          templateMap.set(node.componentChunkName, pageList);
-        })
-    ).then(() => {
-      if (fs.existsSync("seo-export/")) {
-        rimraf.sync("seo-export");
-      }
-      fs.mkdirSync("seo-export");
-      for (const [component, list] of templateMap.entries()) {
-        list.sort((a, b) => a.path.localeCompare(b.path));
-        const csv = convertArrayToCSV(list);
-        fs.writeFileSync(`seo-export/${component}.csv`, csv);
-      }
-    });
+        const pageList = templateMap.get(node.componentChunkName) || [];
+        pageList.push({
+          path: node.path,
+          title: $("head > title")
+            .contents()
+            .text(),
+          description: $("meta[name='description']").attr("content"),
+          keywords: $("meta[name='keywords']").attr("content")
+        });
+        templateMap.set(node.componentChunkName, pageList);
+      });
+
+    const worker = async () => {
+      let a;
+      do {
+        a = promises.pop();
+        if (a) {
+          await a();
+        }
+      } while (a);
+    };
+
+    await Promise.all(Array.from({ length: 10 }).map(worker));
+
+    if (fs.existsSync("seo-export/")) {
+      rimraf.sync("seo-export");
+    }
+    fs.mkdirSync("seo-export");
+    for (const [component, list] of templateMap.entries()) {
+      list.sort((a, b) => a.path.localeCompare(b.path));
+      const csv = convertArrayToCSV(list);
+      fs.writeFileSync(`seo-export/${component.replace('component---', '')}.csv`, csv);
+    }
   }
 }
 
